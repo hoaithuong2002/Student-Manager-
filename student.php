e0b3af9a5c3a39a663f4844f2f28b3356f998bea
hoaithuong2002 <dgth2o2@gmail.com>
2021-01-04 11:34:34 +0700

    thông tin

--- student.php
+++ student.php
@@ -3,9 +3,27 @@
 
 class student
 {
-   protected $id;
-   protected $name;
-   protected $date;
-   protected $sex;
+    protected $id;
+    protected $name;
+    protected $date;
+    protected $sex;
+    protected  $from;
+
+    /**
+     * student constructor.
+     * @param $id
+     * @param $name
+     * @param $date
+     * @param $sex
+     * @param $from
+     */
+    public function __construct($id, $name, $date, $sex, $from)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->date = $date;
+        $this->sex = $sex;
+        $this->from = $from;
+    }
 
 }
