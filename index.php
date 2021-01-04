af584d274daa5d3473157f534affb2c85a7cc059
hoaithuong2002 <dgth2o2@gmail.com>
2021-01-04 14:43:38 +0700

    nejwighfuehgf

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<form >
+    <fieldset>
+        <legend>Student</legend>
+        ID:          <input type="text" name="id" placeholder="nhap ID">
+        Tên:         <input type="text" name="name" placeholder="Nhap Ten">
+        Ngày sinh :  <input type="date" name="date" placeholder="Nhap sinh">
+
+    </fieldset>
+</form>
+</body>
+</html><?php
--- student.php
+++ student.php
@@ -6,7 +6,7 @@ class student
     protected $id;
     protected $name;
     protected $date;
-    protected $sex;
+    protected $gender;
     protected  $from;
 
     /**
@@ -14,15 +14,15 @@ class student
      * @param $id
      * @param $name
      * @param $date
-     * @param $sex
+     * @param $gender
      * @param $from
      */
-    public function __construct($id, $name, $date, $sex, $from)
+    public function __construct($id, $name, $date, $gender, $from)
     {
         $this->id = $id;
         $this->name = $name;
         $this->date = $date;
-        $this->sex = $sex;
+        $this->gender = $gender;
         $this->from = $from;
     }
 
